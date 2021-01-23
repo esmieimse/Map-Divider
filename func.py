06bd6994d4e5035f1d7ca57b335fe6e89d54cd77
esmie <75773842+esmieimse@users.noreply.github.com>
2021-01-23 17:09:38 +0900

    first commit

---
+++ func.py
@@ -0,0 +1,197 @@
+import cv2
+import numpy as np
+
+# 左辺を決める
+def calcX1(mask, width, height):
+  i = flag = 0
+  ref_y = int(height/2)
+
+  while True:
+    if mask[ref_y, i] > 0: flag = 1
+    elif (flag == 1):
+      if mask[ref_y, i] == 0: 
+        X1 = i
+        break
+    elif i > int(width/2): 
+      X1 = 0
+      break
+    i = i+1
+
+  return X1
+
+# 上辺を決める
+def calcY1(mask, width, height):
+  i = flag = 0
+  ref_x = int(width/3)
+
+  while True:
+    if mask[i, ref_x] > 0: flag = 1
+    elif (flag == 1):
+      if mask[i, ref_x] == 0: 
+        Y1 = i
+        break
+    elif i > int(height/2): 
+      Y1 = 0
+      break
+    i = i+1
+
+  return Y1
+
+def calcY1_cf(mask1, mask2, width, height):
+  i = flag = 0
+  ref_x = int(width/3)
+
+  while True:
+    if mask2[i, ref_x] > 0: 
+      flag = 1
+    elif mask1[i, ref_x] > 0 and flag == 1: 
+      flag = 2
+    elif flag == 2:
+      if mask1[i, ref_x] == 0: 
+        Y1 = i
+        break
+    elif i > int(height/2): 
+      Y1 = 0
+      break
+    i = i+1
+
+  return Y1
+
+# 右辺を決める
+def calcX2(mask, width, height):
+  i = flag = 0
+  ref_y = int(height/3)
+
+  while True:
+    j = (width-1) - i
+    if mask[ref_y, j] > 0: flag = 1
+    elif (flag == 1):
+      if mask[ref_y, j] == 0: 
+        X2 = j
+        break
+    elif i > int(width/2): 
+      X2 = width - 1
+      break
+    i = i+1
+
+  return X2
+
+
+# cv2.imreadのパスを日本語可にする
+def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
+  try:
+    n = np.fromfile(filename, dtype)
+    img = cv2.imdecode(n, flags)
+    return img
+  except Exception as e:
+    print(e)
+    return None
+
+def adjust(img, alpha, beta):
+  # 積和演算を行う。
+  dst = alpha * img + beta
+  # [0, 255] でクリップし、uint8 型にする。
+  return np.clip(dst, 0, 255).astype(np.uint8)
+
+
+# マップ部分のトリミングとサイズ調整
+def createBase(filepath, series, alpha=1.0, beta=0.0):
+  img = imread(filepath)
+
+  height = img.shape[0]
+  width = img.shape[1]
+
+  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+  #OpenCVのHSVはH:0-180 / S,V:0-255
+
+  if series == "wwds":
+    color_lo=np.array([25,100,150])
+    color_hi=np.array([35,160,190])
+    mask=cv2.inRange(hsv,color_lo,color_hi)
+
+    X1 = calcX1(mask, width, height)
+    Y1 = calcY1(mask, width, height)
+    X2 = calcX2(mask, width, height)
+
+  elif series == "wwvc":
+    color_lo=np.array([27,150,90])
+    color_hi=np.array([38,255,140])
+    mask=cv2.inRange(hsv,color_lo,color_hi)
+    
+    X1 = calcX1(mask, width, height)
+    Y1 = calcY1(mask, width, height)
+    X2 = calcX2(mask, width, height)
+
+  elif series == "cf":
+    color_lo_1=np.array([60,60,150])
+    color_hi_1=np.array([90,160,250])
+    color_lo_2=np.array([80,220,30])
+    color_hi_2=np.array([95,255,95])
+
+    mask1=cv2.inRange(hsv,color_lo_1,color_hi_1)
+    mask2=cv2.inRange(hsv,color_lo_2,color_hi_2)
+
+    X1 = calcX1(mask1, width, height)
+    Y1 = calcY1_cf(mask1, mask2, width, height)
+    X2 = calcX2(mask1, width, height)
+
+  Y2 = Y1 + X2 - X1
+
+  img_crop = img[Y1: Y2+1, X1 : X2+1]
+
+  if series in {"wwds", "wwvc"}:
+    img_resize = cv2.resize(img_crop, (256,256), interpolation=cv2.INTER_NEAREST)
+  elif series in {"cf"}:
+    img_resize = cv2.resize(img_crop, (280,280), interpolation=cv2.INTER_NEAREST)
+
+  img_adjust = adjust(img_resize, alpha, beta)
+
+  return img_adjust
+
+
+# 市松模様
+def addFill(img, series):
+  depth = 50
+  
+  if series in {"wwds", "wwvc"}:
+    black = np.full((64, 64, 3), np.uint8(depth))
+    white = np.full((64, 64, 3), np.uint8(0))
+
+    f1 = np.concatenate([black, white, black, white], 1)
+    f2 = np.concatenate([white, black, white, black], 1)
+    f  = np.concatenate([f1, f2, f1, f2], 0)
+
+  elif series in {"cf"}:
+    black = np.full((56, 56, 3), np.uint8(depth))
+    white = np.full((56, 56, 3), np.uint8(0))
+
+    f1 = np.concatenate([black, white, black, white, black], 1)
+    f2 = np.concatenate([white, black, white, black, white], 1)
+    f  = np.concatenate([f1, f2, f1, f2, f1], 0)
+
+  img_fill = cv2.subtract(img, f) 
+
+  return img_fill
+
+
+# 格子
+def addGrid(img, series):
+  img_grid = img
+
+  if series in {"wwds", "wwvc"}:
+    for c in range(3):
+      for x in range(63, 256, 64):
+        for y in range(256):
+          img_grid[x, y, c] = 255
+          img_grid[y, x, c] = 255
+  elif series in {"cf"}:
+    for c in range(3):
+      for x in range(55, 280, 56):
+        for y in range(280):
+          img_grid[x, y, c] = 255
+          img_grid[y, x, c] = 255
+
+  return img_grid
+
+
---
+++ main.py
@@ -0,0 +1,260 @@
+import func
+import PySimpleGUI as sg
+import glob
+import os
+import configparser
+import cv2
+import sys
+import win32api
+
+def resource_path(relative_path):
+  if hasattr(sys, '_MEIPASS'):
+    basepath = win32api.GetLongPathName(sys._MEIPASS)
+    return os.path.join(basepath, relative_path)
+  return os.path.join(os.path.abspath("."), relative_path)
+
+
+config = configparser.ConfigParser()
+config.read('MapDivider.config', encoding='utf-8')
+
+
+sg.theme('SystemDefault')
+
+seriesTitle1 = sg.Combo(('ACWW (DS)', 'ACWW (WiiU)', 'ACCF'), default_value='シリーズを選択', size=(15, 1), key='-SERIES1-')
+seriesTitle2 = sg.Combo(('ACWW (DS)', 'ACWW (WiiU)', 'ACCF'), default_value='シリーズを選択', size=(15, 1), key='-SERIES2-')
+errorMessage1s = sg.Text(size=(15, 1), text_color='red', key='-ERROR1s-')
+errorMessage1f = sg.Text(size=(15, 1), text_color='red', key='-ERROR1f-')
+errorMessage2s = sg.Text(size=(15, 1), text_color='red', key='-ERROR2s-')
+errorMessage2f = sg.Text(size=(15, 1), text_color='red', key='-ERROR2f-')
+
+checkFill1 = sg.Checkbox('色分け', default=config['OVERLAY']['Fill'], key='-FILL1-')
+checkFill2 = sg.Checkbox('色分け', default=config['OVERLAY']['Fill'], key='-FILL2-')
+checkGrid1 = sg.Checkbox('グリッド', default=config['OVERLAY']['Grid'], key='-GRID1-')
+checkGrid2 = sg.Checkbox('グリッド', default=config['OVERLAY']['Grid'], key='-GRID2-')
+
+checkCol1 = [[checkFill1], [checkGrid1], [sg.Submit('実行', size=(10,1), key='-SUBMIT1-')]]
+checkCol2 = [[checkFill2], [checkGrid2], [sg.Submit('実行', size=(10,1), key='-SUBMIT2-')]]
+
+Image1 = sg.Image(size=(256,256), pad=((30,10),10), background_color='#d5d8d8', key='-IMAGE1-')
+Image2 = sg.Image(size=(256,256), pad=((30,10),10), background_color='#d5d8d8', key='-IMAGE2-')
+
+Slider1a = sg.Slider(range=(1, 1.5), size=(28, 12), default_value=config['DISPLAY']['Alpha'], resolution=0.05, pad=(0,0), 
+                     orientation='h', disable_number_display=True, enable_events=True, disabled = True, key='-SLIDER1a-')
+Slider1b = sg.Slider(range=(-30, 30), size=(28, 12), default_value=config['DISPLAY']['Beta'], resolution=6, pad=(0,0), 
+                     orientation='h', disable_number_display=True, enable_events=True, disabled = True, key='-SLIDER1b-')
+Slider2a = sg.Slider(range=(1, 1.5), size=(28, 12), default_value=config['DISPLAY']['Alpha'], resolution=0.05, pad=(0,0), 
+                     orientation='h', disable_number_display=True, enable_events=True, disabled = True, key='-SLIDER2a-')
+Slider2b = sg.Slider(range=(-30, 30), size=(28, 12), default_value=config['DISPLAY']['Beta'], resolution=6, pad=(0,0), 
+                     orientation='h', disable_number_display=True, enable_events=True, disabled = True, key='-SLIDER2b-')
+
+Slider1 = [[sg.Text('明るさ', size=(6,1), pad=((4,3),2)), Slider1b], [sg.Text('ｺﾝﾄﾗｽﾄ', size=(6,1), pad=((4,3),2)), Slider1a]]
+Reset1 = sg.Button('リセット', pad=((6,11),0), button_color=('black','#dddddd'), enable_events=True, key='-RESET1-')
+Settings1 = sg.Frame('表示設定',[[sg.Col(Slider1), Reset1]] ,title_color='#334444', pad=(5,10))
+
+Slider2 = [[sg.Text('明るさ', size=(6,1), pad=((4,3),2)), Slider2b], [sg.Text('ｺﾝﾄﾗｽﾄ', size=(6,1), pad=((4,3),2)), Slider2a]]
+Reset2 = sg.Button('リセット', pad=((6,11),0), button_color=('black','#dddddd'), enable_events=True, key='-RESET2-')
+Settings2 = sg.Frame('表示設定',[[sg.Col(Slider2), Reset2]] ,title_color='#334444', pad=(5,10))
+
+
+layoutTab1 = [
+  [seriesTitle1, errorMessage1s],
+  [sg.Text('画像ファイルを選択：', pad=((5,0),(5,0))), errorMessage1f],
+  [sg.InputText(size=(45,1), default_text=config['GENERAL']['FolderPath'], key='-FOLDERPATH-'), sg.FolderBrowse()], 
+  [sg.Text('_' * 58, text_color='#aaaaaa')],
+  [sg.Frame('', [[Image1,sg.Col(checkCol1)]], background_color='#d5d8d8', element_justification = "center", pad=((5,5),(5,0)))],
+  [Settings1]
+]
+
+layoutTab2 = [
+  [seriesTitle2, errorMessage2s],
+  [sg.Text('画像ファイルを選択：', pad=((5,0),(5,0))), errorMessage2f],
+  [sg.InputText(size=(45,1), key='-FILEPATH-'), sg.FileBrowse(file_types=(('Image Files', ['*.png', '*.jpg', '*.jpeg', '*.bmp']),))], 
+  [sg.Text('_' * 58, text_color='#aaaaaa')],
+  [sg.Frame('', [[Image2,sg.Col(checkCol2)]], background_color='#d5d8d8', element_justification = "center", pad=((5,5),(5,0)))],
+  [Settings2]
+]
+
+layout = [
+  [sg.TabGroup([[sg.Tab('指定フォルダから生成', layoutTab1), sg.Tab('画像から生成', layoutTab2)]])]
+]
+
+
+window = sg.Window('マップ区画表示ツール', layout, icon=resource_path('app.ico'), titlebar_icon='app.ico')
+
+
+flag1 = False
+flag2 = False
+
+while True:
+  event, values = window.read()
+
+  if event is None:
+    if flag1 == True:
+      alpha = str(alpha)
+      beta  = str(beta)
+      config.set('DISPLAY', 'alpha', alpha)
+      config.set('DISPLAY', 'beta', beta)
+    elif flag2 == True:
+      alpha = str(alpha)
+      beta  = str(beta)
+      config.set('DISPLAY', 'alpha', alpha)
+      config.set('DISPLAY', 'beta', beta)
+
+    with open('MapDivider.config', 'w') as configfile:
+      config.write(configfile)
+      
+    break
+
+
+  ##実行ボタン押したときの処理(Tab1)
+  if event == '-SUBMIT1-':
+    if values['-SERIES1-'] == 'シリーズを選択':
+      window['-ERROR1s-'].update('選択してください')
+
+    elif values['-FOLDERPATH-'] == '':
+      window['-ERROR1s-'].update('')
+      window['-ERROR1f-'].update('選択してください')
+      
+    else:
+      window['-ERROR1f-'].update('')
+      if values['-SERIES1-'] == 'ACWW (DS)':
+        series = 'wwds'
+      elif values['-SERIES1-'] == 'ACWW (WiiU)':
+        series = 'wwvc'
+      elif values['-SERIES1-'] == 'ACCF':
+        series = 'cf'
+  
+      config.set('GENERAL', 'FolderPath', values['-FOLDERPATH-'])
+      files = glob.glob(values['-FOLDERPATH-'] + '/*')
+      files = sorted(files, key=lambda f: os.stat(f).st_ctime, reverse=True)
+
+
+      alpha = values['-SLIDER1a-']
+      beta  = values['-SLIDER1b-']
+      img = func.createBase(files[0], series, alpha, beta)
+
+      if values['-FILL1-'] == True:
+        img = func.addFill(img, series)
+        config.set('OVERLAY', 'Fill', '1')
+      else:
+        config.set('OVERLAY', 'Fill', '0')
+
+      if values['-GRID1-'] == True:
+        img = func.addGrid(img, series)
+        config.set('OVERLAY', 'Grid', '1')
+      else:
+        config.set('OVERLAY', 'Grid', '0')
+      
+      img_encode = cv2.imencode('.png', img)[1].tobytes()
+      window['-IMAGE1-'].update(data=img_encode)
+      window['-SLIDER1a-'].update(disabled = False)
+      window['-SLIDER1b-'].update(disabled = False)
+
+
+  ##実行ボタン押したときの処理(Tab2)
+  if event == '-SUBMIT2-':
+    if values['-SERIES2-'] == 'シリーズを選択':
+      window['-ERROR2s-'].update('選択してください')
+
+    elif values['-FILEPATH-'] == '':
+      window['-ERROR2s-'].update('')
+      window['-ERROR2f-'].update('選択してください')
+      
+    else:
+      window['-ERROR2f-'].update('')
+      if values['-SERIES2-'] == 'ACWW (DS)':
+        series = 'wwds'
+      elif values['-SERIES2-'] == 'ACWW (WiiU)':
+        series = 'wwvc'
+      elif values['-SERIES2-'] == 'ACCF':
+        series = 'cf'
+
+      alpha = values['-SLIDER2a-']
+      beta  = values['-SLIDER2b-']
+      img = func.createBase(values['-FILEPATH-'], series, alpha, beta)
+
+      if values['-FILL2-'] == True:
+        img = func.addFill(img, series)
+        config.set('OVERLAY', 'Fill', '1')
+      else:
+        config.set('OVERLAY', 'Fill', '0')
+
+      if values['-GRID2-'] == True:
+        img = func.addGrid(img, series)
+        config.set('OVERLAY', 'Grid', '1')
+      else:
+        config.set('OVERLAY', 'Grid', '0')
+      
+      img_encode = cv2.imencode('.png', img)[1].tobytes() 
+      window['-IMAGE2-'].update(data=img_encode)
+      window['-SLIDER2a-'].update(disabled = False)
+      window['-SLIDER2b-'].update(disabled = False) 
+
+  
+  ##スライダー動かしたときの処理(Tab1)
+  if event in ['-SLIDER1a-', '-SLIDER1b-']:
+    flag1 = True
+    alpha = values['-SLIDER1a-']
+    beta  = values['-SLIDER1b-']
+    img = func.createBase(files[0], series, alpha, beta)
+
+    if values['-FILL1-'] == True:
+      img = func.addFill(img, series)
+    if values['-GRID1-'] == True:
+      img = func.addGrid(img, series)
+    
+    img_encode = cv2.imencode('.png', img)[1].tobytes()
+    window['-IMAGE1-'].update(data=img_encode)
+
+
+  ##スライダー動かしたときの処理(Tab2)
+  if event in ['-SLIDER2a-', '-SLIDER2b-']:
+    flag2 = True
+    alpha = values['-SLIDER2a-']
+    beta  = values['-SLIDER2b-']
+    img = func.createBase(values['-FILEPATH-'], series, alpha, beta)
+
+    if values['-FILL2-'] == True:
+      img = func.addFill(img, series)
+    if values['-GRID2-'] == True:
+      img = func.addGrid(img, series)
+    
+    img_encode = cv2.imencode('.png', img)[1].tobytes()
+    window['-IMAGE2-'].update(data=img_encode)
+
+
+  ##スライダーリセット処理(Tab1)
+  if event == '-RESET1-':
+    window.FindElement('-SLIDER1a-').Update(1.0)
+    window.FindElement('-SLIDER1b-').Update(0.0)
+
+    img = func.createBase(files[0], series)
+
+    if values['-FILL1-'] == True:
+      img = func.addFill(img, series)
+    if values['-GRID1-'] == True:
+      img = func.addGrid(img, series)
+    
+    img_encode = cv2.imencode('.png', img)[1].tobytes()
+    window['-IMAGE1-'].update(data=img_encode)
+
+
+  ##スライダーリセット処理(Tab2)
+  if event == '-RESET2-':
+    window.FindElement('-SLIDER2a-').Update(1.0)
+    window.FindElement('-SLIDER2b-').Update(0.0)
+    
+    img = func.createBase(values['-FILEPATH-'], series)
+
+    if values['-FILL2-'] == True:
+      img = func.addFill(img, series)
+    if values['-GRID2-'] == True:
+      img = func.addGrid(img, series)
+    
+    img_encode = cv2.imencode('.png', img)[1].tobytes()
+    window['-IMAGE2-'].update(data=img_encode)
+
+
+
+window.close()
+print(values)
